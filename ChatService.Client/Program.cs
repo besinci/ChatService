ed2c5f6e8ab5d937746796aaf46c68a6c0969d7b
Ali Beşinci <aliosmanbesinci@gmail.com>
2020-09-04 02:21:34 +0300

    Add GenerateClientId method.

--- ChatService.Client/Program.cs
+++ ChatService.Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace ChatService.Client
     {
         static void Main(string[] args)
         {
+            var clientId = GenerateClientId();
+            Console.Title = $"The Client #{ clientId }";
+
+
+        }
+
+        private static int GenerateClientId()
+        {
+            return new Random().Next(0, 100);
         }
     }
 }
