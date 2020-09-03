ce54af70e1181ab3fba5f02c0e19b5ccbdace41c
Ali Beşinci <aliosmanbesinci@gmail.com>
2020-09-04 02:52:05 +0300

    Refactor for Client class.

--- ChatService.Client/Client.cs
+++ ChatService.Client/Client.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ChatService.Infrastructure;
+using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
+
 
 namespace ChatService.Client
 {
-    public class Client
+    public class Client : BaseService
     {
         public int ID { get; private set; }
-        public int PORT { get; private set; }
-        public Socket ClientSocket { get; private set; }
 
         public Client(int port)
         {
             ID = new Random().Next(0, 100);
             PORT = port;
-            ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
         public void Connect()
         {
-            while (ClientSocket.Connected == false)
+            while (Socket.Connected == false)
             {
                 try
                 {
-                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                    Socket.Connect(IPAddress.Loopback, PORT);
                 }
                 catch (Exception ex)
                 {
